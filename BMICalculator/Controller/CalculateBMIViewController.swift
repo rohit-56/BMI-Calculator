69a9474db586c10707264f8235a40c17e4e5260e
rohit-56 <rohitmou25@gmail.com>
2022-11-27 17:10:53 +0530

    implement ui & viewController for calculate BMI

--- BMICalculator/Controller/CalculateBMIViewController.swift
+++ BMICalculator/Controller/CalculateBMIViewController.swift
@@ -9,21 +9,24 @@ import UIKit
 
 class CalculateBMIViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    
+    
+    @IBOutlet weak var weightLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+     @IBAction func heightSlider(_ sender: UISlider) {
+         heightLabel.text = String(format: "%.2f", sender.value)+"m"
+     }
+    
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.2f", sender.value)+"Kg"
     }
-    */
-
+    
 }
