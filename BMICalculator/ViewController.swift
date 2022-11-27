57e5f387fb2b9e5b08364372570abcf4a450028e
rohit-56 <rohitmou25@gmail.com>
2022-11-27 14:03:39 +0530

    launch screen with gradient and heading timer

--- BMICalculator/ViewController.swift
+++ BMICalculator/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backImage: UIImageView!
+    @IBOutlet weak var bmi: UILabel!
+    @IBOutlet weak var calculator: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setTimerInBMILabel()
+        setBackgroundGradient()
     }
-
-
+    
+    //MARK: Function to set gradient in Background
+    func setBackgroundGradient(){
+        var gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.systemRed.cgColor , UIColor.purple.cgColor]
+        gradientLayer.frame = view.bounds
+        backImage.layer.addSublayer(gradientLayer)
+    }
+    
+    //MARK: This function used to set timer for every letter in BMI Label
+    func setTimerInBMILabel(){
+        bmi.text = ""
+        calculator.text = ""
+        let bmiText = "BMI"
+        var index = 0
+        for letter in bmiText {
+            Timer.scheduledTimer(withTimeInterval: 0.3 * Double(index), repeats: false){(timer) in
+                self.bmi.text?.append(letter)
+            }
+            index+=1
+        }
+        
+        index = 0
+        let calculatorText = "Calculator"
+        
+        for letter in calculatorText {
+            Timer.scheduledTimer(withTimeInterval: 0.3 * Double(index), repeats: false){(timer) in
+                self.calculator.text?.append(letter)
+            }
+            index+=1
+        }
+    }
 }
 
